c4fc3c36190ef5f6d5ea7da07f89d530ef065f6e
unknown <jugoslavsubotic@gmail.com>
2021-07-09 09:41:56 +0200

    adding more JS

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -12,6 +12,18 @@ const hamburger_menu = document.querySelector('.hamburger-menu')
 const menu_closed = document.querySelector('.menu-closed')
 const mobile_nav = document.querySelector('.mobile-nav')
 
+const mobile_product = document.querySelector('.mobile-product')
+const mobile_product_arrow = document.getElementById('mobile-product-arrow')
+const product_mobile_menu = document.querySelector('.product-mobile-menu')
+
+const mobile_company = document.querySelector('.mobile-company')
+const mobile_company_arrow = document.getElementById('mobile-company-arrow')
+const company_mobile_menu = document.querySelector('.company-mobile-menu')
+
+const mobile_connect = document.querySelector('.mobile-connect')
+const mobile_connect_arrow = document.getElementById('mobile-connect-arrow')
+const connect_mobile_menu = document.querySelector('.connect-mobile-menu')
+
 product.addEventListener('click', function() {
     nav_product.classList.toggle('hidden')
     product_icon.classList.toggle('rotated')
@@ -38,3 +50,20 @@ menu_closed.addEventListener('click', function() {
     hamburger_menu.classList.remove('hidden')
     mobile_nav.classList.remove('active')
 })
+
+mobile_product.addEventListener('click', function() {
+    product_mobile_menu.classList.toggle('hidden')
+    mobile_product_arrow.classList.toggle('rotated')
+})
+
+mobile_company.addEventListener('click', function() {
+    company_mobile_menu.classList.toggle('hidden')
+    mobile_company_arrow.classList.toggle('rotated')
+})
+
+mobile_connect.addEventListener('click', function() {
+    connect_mobile_menu.classList.toggle('hidden')
+    mobile_connect_arrow.classList.toggle('rotated')
+})
+
+
