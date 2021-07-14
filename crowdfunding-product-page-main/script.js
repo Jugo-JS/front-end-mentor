3b850f76602f1831500124f660afc40b7dcf08e3
unknown <jugoslavsubotic@gmail.com>
2021-07-14 14:47:45 +0200

    adding modal functionalities

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -7,6 +7,14 @@ const circle = document.querySelector('.circle')
 const path = document.querySelector('.path')
 const bookmarkButton = document.getElementById('bookmark-button')
 
+const backButton = document.querySelector('.back-project')
+const selectionModal = document.querySelector('.modal')
+const closeSelectionModal = document.querySelector('.close-modal')
+
+const modalCards = document.querySelectorAll('.modal-card')
+const labels = document.querySelectorAll('.label')
+const modalCardBottom = document.querySelectorAll('.modal-card-bottom')
+
 hamburger.addEventListener('click', function() {
     mobileMenu.style.display = 'block'
     hamburger.style.display = 'none'
@@ -27,4 +35,30 @@ bookmarkButton.addEventListener('click', function() {
     bookmarkButton.style.fontWeight = '700'
     bookmarkButton.style.textAlign = 'right'
     bookmarkButton.textContent = 'Bookmarked'
+})
+
+backButton.addEventListener('click', function() {
+    selectionModal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+    closeSelectionModal.addEventListener('click', function() {
+        selectionModal.style.display = 'none'
+        document.body.style.overflow = 'visible'
+    })
+})
+
+labels.forEach((label, index) => {
+    label.addEventListener('click', function() {
+        modalCardBottom.forEach((item, i) => {
+            if(index === i) {
+                item.classList.toggle('active')
+                modalCards.forEach((card, j) => {
+                    if(j === i && item.classList.contains('active')) {
+                        card.style.border = '2px solid hsl(176, 50%, 47%)'
+                    } else {
+                        card.style.border = '1px solid rgb(235, 235, 235)'
+                    }
+                })
+            } 
+        })
+    })
 })
