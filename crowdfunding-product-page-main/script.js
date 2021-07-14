8878e68fe0ae6871315d443092a2791eb4f54301
unknown <jugoslavsubotic@gmail.com>
2021-07-14 11:50:28 +0200

    adding bookmark button

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -3,6 +3,8 @@ const closeMenu = document.querySelector('.close-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
 const bookmarkIcon = document.querySelector('.bookmark-icon')
+const circle = document.querySelector('.circle')
+const path = document.querySelector('.path')
 const bookmarkButton = document.getElementById('bookmark-button')
 
 hamburger.addEventListener('click', function() {
@@ -19,5 +21,10 @@ hamburger.addEventListener('click', function() {
 })
 
 bookmarkButton.addEventListener('click', function() {
-    console.log('click')
+    circle.style.fill = 'hsl(176, 72%, 28%)'
+    path.style.fill = 'white'
+    bookmarkButton.style.color = 'hsl(176, 72%, 28%)'
+    bookmarkButton.style.fontWeight = '700'
+    bookmarkButton.style.textAlign = 'right'
+    bookmarkButton.textContent = 'Bookmarked'
 })
