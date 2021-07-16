4ec878c24a4e702808e8dea3969e74b52ccf446d
unknown <jugoslavsubotic@gmail.com>
2021-07-16 13:27:14 +0200

    finishing last functionalities

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -22,8 +22,18 @@ const continueBtns = document.querySelectorAll('.continueBtn')
 const successModal = document.querySelector('.success-modal')
 const finishBtn = document.querySelector('.finish')
 
-const collectedMoney = Number(document.getElementById('money').textContent)
+let collectedMoney = Number(document.getElementById('money').textContent)
 const totalAmount = Number(document.getElementById('totalAmount').textContent)
+let backers = Number(document.getElementById('backers').textContent)
+const noReward = document.getElementById('no-reward')
+const bamboo = document.getElementById('bamboo')
+const black = document.getElementById('black')
+const blueLine = document.querySelector('.blue-line')
+
+let bambooLeft = Number(document.getElementById('bamboo-left').textContent)
+let blackLeft = Number(document.getElementById('black-left').textContent)
+let modalBambooLeft = Number(document.getElementById('modal-bamboo-left').textContent)
+let modalBlackLeft = Number(document.getElementById('modal-black-left').textContent)
 
 hamburger.addEventListener('click', function() {
     mobileMenu.style.display = 'block'
@@ -89,11 +99,33 @@ selectBtns.forEach(btn => {
     btn.addEventListener('click', mainModal)
 })
 
-continueBtns.forEach(btn => {
+continueBtns.forEach((btn, i) => {
     btn.addEventListener('click', function() {
         selectionModal.style.display = 'none'
         successModal.style.display = 'block'
          document.body.style.overflow = 'hidden'
+         if(i === 0) {
+             collectedMoney += Number(noReward.value)
+             backers += 1
+         } else if(i === 1) {
+             collectedMoney += Number(bamboo.value)
+             backers += 1
+             bambooLeft -= 1
+             modalBambooLeft -= 1
+         } else if (i === 2) {
+             collectedMoney += Number(black.value)
+             backers += 1
+             blackLeft -= 1
+             modalBlackLeft -= 1
+         }
+         document.getElementById('money').textContent = collectedMoney
+         document.getElementById('backers').textContent = backers
+         document.getElementById('bamboo-left').textContent = bambooLeft
+         document.getElementById('black-left').textContent = blackLeft
+         document.getElementById('modal-bamboo-left').textContent = modalBambooLeft
+         document.getElementById('modal-black-left').textContent = modalBlackLeft
+         let lineWidth = (collectedMoney/totalAmount) * 100
+         blueLine.style.width = `${lineWidth.toString()}%`
     })
 })
 
